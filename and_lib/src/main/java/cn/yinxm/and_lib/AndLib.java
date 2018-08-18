8d295af722c3d19bce5ce8e9f60e6a63156b6c3f
yinxuming <yinxuming@baidu.com>
2018-08-19 00:43:44 +0800

    更新库

---
+++ and_lib/src/main/java/cn/yinxm/and_lib/AndLib.java
@@ -0,0 +1,20 @@
+
+
+package cn.yinxm.and_lib;
+
+/**
+ * <p>
+ *
+ * @author yinxuming
+ * @date 2018/8/19
+ */
+public class AndLib {
+
+    public static void test () {
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
