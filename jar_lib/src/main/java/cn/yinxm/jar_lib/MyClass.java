881a3766f90a501169e10c215c8dc0edd5062660
yinxuming <yinxuming@baidu.com>
2018-08-18 23:44:42 +0800

    新增lib库

---
+++ jar_lib/src/main/java/cn/yinxm/jar_lib/MyClass.java
@@ -0,0 +1,13 @@
+
+package cn.yinxm.jar_lib;
+
+public class MyClass {
+
+    public static void testFunc1() {
+        System.out.println("testFunc1");
+    }
+
+    public static void main (String [] args) {
+        testFunc1();
+    }
+}
