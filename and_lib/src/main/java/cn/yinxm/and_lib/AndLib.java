01362d08ea00ab186d40c49c38e6a79ecc2747b5
yinxuming <yinxuming@baidu.com>
2018-08-19 22:56:43 +0800

    还原项目程序

--- and_lib/src/main/java/cn/yinxm/and_lib/AndLib.java
+++ and_lib/src/main/java/cn/yinxm/and_lib/AndLib.java
@@ -14,6 +14,25 @@ public class AndLib {
         System.out.println("test");
     }
 
+    public static void test2() {
+        System.out.println("test2");
+    }
+
+    public static void test3() {
+        System.out.println("test3");
+    }
+
+    public static void test4() {
+        System.out.println("test4");
+    }
+
+    public static void test5() {
+        System.out.println("test5");
+    }
+    public static void test6() {
+        System.out.println("test6");
+    }
+
     public static void main(String[] args) {
         test();
     }
